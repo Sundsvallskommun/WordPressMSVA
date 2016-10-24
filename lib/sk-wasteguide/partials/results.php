6087f0dd77c7bb65c6a0d367b6ae467130bb2f39
Andreas Färnstrand <andreas.farnstrand@cybercom.com>
2016-10-24 12:57:44 +0200

    Cleanup and fix to work with rewritten search module results

--- lib/sk-wasteguide/partials/results.php
+++ lib/sk-wasteguide/partials/results.php
@@ -8,30 +8,19 @@ $pagenum = get_query_var('paged', 1);
 $module_i = 0;
 $module_count = count($results);
 
-echo '<div class="col-lg-12">';
+echo '<div class="col-lg-12">'; ?>
 
-foreach ($results as $type => $result): ?>
+<?php $result = $results['material']; ?>
 
+<?php if ( isset( $results['material'] ) ) : ?>
 <?php
-	//echo '<pre>' . print_r( $result['found_posts'], true ) . '</pre>';
-	if( $result['found_posts'] > 0 ) {
 
-		if ( $result['posts'][0]['type'] != 'material' ) {
-			continue;
-		}
-
-	} else {
-
-		continue;
-
-	}
 	$module_i += 1;
 
 	if($module_i == ceil($module_count / 2) + 1) {
 		echo '</div>';
 		echo '<div class="col-lg-6">';
 	}
-
 
 ?>
 
@@ -60,90 +49,58 @@ foreach ($results as $type => $result): ?>
 		<ol class="search-module__items">
 
 		<?php foreach ($result['posts'] as $post): ?>
-			<li>
+
 			<?php
 				$post_data = get_post( $post['id'] );
 				$sorting_terms = wp_get_post_terms( $post['id'], 'material_sorting' );
 				$leave_terms = wp_get_post_terms( $post['id'], 'material_deposit' );
 
-				?>
+			?>
+			<div class="card">
+				<div class="card-header" role="tab" id="headingOne">
+					<h3 class="mb-0">
+						<?php echo $post_data->post_title; ?>
+					</h3>
+				</div>
 
-				<!-- <h3 class="search-module__item__title"><?php echo $post_data->post_title; ?></h3> -->
+				<div id="collapseOne" class="collapse in" role="tabpanel" aria-labelledby="headingOne">
+					<div class="card-block">
 
-
-
-				<div class="card">
-					<div class="card-header" role="tab" id="headingOne">
-						<h5 class="mb-0">
-							<?php echo $post_data->post_title; ?>
-						</h5>
-					</div>
-
-					<div id="collapseOne" class="collapse in" role="tabpanel" aria-labelledby="headingOne">
-						<div class="card-block">
-
+						<div class="">
+							<h4>Sorteras som...</h4>
 							<div class="">
-								<h6>Sorteras som...</h6>
-								<div class="">
-									<?php foreach( $sorting_terms as $term ) : ?>
-										<strong><?php echo $term->name; ?></strong>
-										<?php echo get_field( 'beskrivning', $term ); ?>
-									<?php endforeach; ?>
-								</div>
+								<?php foreach( $sorting_terms as $term ) : ?>
+									<strong><?php echo $term->name; ?></strong>
+									<?php echo get_field( 'beskrivning', $term ); ?>
+								<?php endforeach; ?>
 							</div>
-							<div class="">
-								<h6>Lämnas till...</h6>
-								<div class="">
-									<?php
-									foreach( $leave_terms as $term ) {
-										?>
-										<strong><?php echo $term->name; ?></strong>
-										<?php
-										echo get_field( 'beskrivning', $term );
-									}
-									?>
-								</div>
-							</div>
-
 						</div>
+						<div class="">
+							<h4>Lämnas till...</h4>
+							<div class="">
+								<?php
+								foreach( $leave_terms as $term ) {
+									?>
+									<strong><?php echo $term->name; ?></strong>
+									<?php
+									echo get_field( 'beskrivning', $term );
+								}
+								?>
+							</div>
+						</div>
+
 					</div>
 				</div>
-
-
-
-				<!--<div class="">
-					<div class="" style="">Sorteras som...</div>
-					<div class="">
-						<?php /*foreach( $sorting_terms as $term ) : */?>
-							<div class=""><?php /*echo $term->name; */?></div>
-							<?php /*echo get_field( 'beskrivning', $term ); */?>
-						<?php /*endforeach; */?>
-					</div>
-				</div>
-				<div class="">
-					<div class="">Lämnas till...</div>
-					<div class="">
-						<?php
-/*						foreach( $leave_terms as $term ) {
-							*/?>
-							<div class=""><?php /*echo $term->name; */?></div>
-							<?php
-/*							echo get_field( 'beskrivning', $term );
-						}
-						*/?>
-					</div>
-				</div>-->
-			</li>
-		<?php endforeach; ?>
+			</div>
+		<?php endforeach; // end post loop ?>
 
 		</ol>
-
-			<div class="search-module__footer" data-load-more="<?php echo $type; ?>">
-				<?php if( $result['found_posts'] > $count ) :
+		<div class="search-module__footer" data-load-more="<?php echo $type; ?>">
+			<?php if( $result['found_posts'] > $count ) :
 				next_posts_link( 'Visa fler', 0 );
 				endif;
 			?>
-			</div>
+		</div>
 
 		<?php else: ?>
 			<p class="m-t-2 text-xs-center text-muted">Inget resultat för <?php echo $result['title']; ?></p>
@@ -151,6 +108,6 @@ foreach ($results as $type => $result): ?>
 
 	</div>
 
-<?php endforeach; ?>
+<?php endif; ?>
 
 <?php echo '</div>'; ?>
