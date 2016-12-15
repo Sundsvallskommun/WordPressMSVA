007c250f03b9a8259451bebde94561f1f6af95c5
Andreas Färnstrand <andreas.farnstrand@cybercom.com>
2016-12-15 16:21:47 +0100

    Add support for setting publishing date and time

--- lib/sk-operation-messages/class-sk-operation-messages-ajax.php
+++ lib/sk-operation-messages/class-sk-operation-messages-ajax.php
@@ -30,25 +30,37 @@ class SK_Operation_Messages_Ajax {
 	private function save_message( $params, $errand_text ) {
 
 		// Setup the author, slug, and title for the post
-		$author_id = 1;
-		$slug      = 'example-post';
-
+		$author_id = get_current_user_id();
+		if( $author_id == 0 ) return false; // Not logged in
 
 		$title = $this->create_title( $params['operation_message'] );
+
+		$args = array(
+			'comment_status' => 'closed',
+			'ping_status'    => 'closed',
+			'post_author'    => $author_id,
+			'post_name'      => sanitize_title( $title ),
+			'post_title'     => $title,
+			'post_status'    => 'publish',
+			'post_type'      => 'operation_message'
+		);
+
+		if ( ! empty( $params['operation_message']['om_publish_at_date'] ) ) {
+
+			$string_time = $params['operation_message']['om_publish_at_date'] . ' ' . $params['operation_message']['om_publish_at_hour'] . ':' . $params['operation_message']['om_publish_at_minute'];
+			$publish_time = strtotime( $string_time );
+			$publish_time = date_i18n( 'Y-m-d H:i:s', $publish_time );
+			$publish_time_gmt = date_i18n( 'Y-m-d H:i:s', strtotime( $string_time . ' -1 hours' ) );
+
+			$args['post_date'] = $publish_time;
+			$args['post_date_gmt'] = $publish_time_gmt;
+
+		}
 
 		// If the page doesn't already exist, then create it
 		// Set the post ID so that we know the post was created successfully
 		$post_id = wp_insert_post(
-
-			array(
-				'comment_status' => 'closed',
-				'ping_status'    => 'closed',
-				'post_author'    => $author_id,
-				'post_name'      => sanitize_title( $title ),
-				'post_title'     => $title,
-				'post_status'    => 'publish',
-				'post_type'      => 'operation_message'
-			),
+			$args,
 			true // return error on failure
 		);
 
