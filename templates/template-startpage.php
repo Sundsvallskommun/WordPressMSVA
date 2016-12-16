b2e342deeb9d756cad60f88606bce685cdf6d06f
Daniel Söderström <daniel.soderstrom@cybercom.com>
2016-12-16 15:03:32 +0100

    Search form as designed and some gui.

--- templates/template-startpage.php
+++ templates/template-startpage.php
@@ -11,18 +11,37 @@ $sections = SK_Blocks::get_sections();
 
 <?php // Mobile only ?>
 <?php //get_template_part('partials/boxes'); ?>
-<style>
-	.card{
-		text-align: center;
-		padding: 70px 0;
-	}
+<?php //$temp = SK_Operation_Messages::messages( array( 'publish' ), true );
 
-</style>
+//util::Debug( $temp );
+?>
+<div class="operation-messages">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-8">
 
-
+				<div class="operation-messages-list">
+					<ul>
+						<li class="active"><?php material_icon( 'error', array( 'size' => '2em' ) ); ?> Driftstörning: Hela vårt vatten och avloppsnät och närliggande områden ...</li>
+						<li class="done"><?php material_icon( 'error', array( 'size' => '2em' ) ); ?> Vi har för
+							närvarande inga driftstörningar
+						</li>
+					</ul>
+				</div><!-- .operation-messages-list -->
+			</div>
+			<div class="col-md-4">
+				<div class="operation-messages__button">
+					<a class="btn btn-secondary btn-rounded btn-rounded" href="#"><?php _e( 'Gå till alla driftmeddelanden', 'sk_tivoli' );?></a>
+				</div>
+			</div>
+		</div><!-- .row -->
+	</div><!-- .container-fluid -->
+</div><!-- .operation-messages -->
 
 
 <div class="container-fluid sections">
+
+
 
 	<?php foreach ($sections as $section ) : ?>
 	<div class="row blocks<?php SK_Blocks::is_grid_border( $section ); ?>">
