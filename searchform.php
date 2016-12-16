26ce16642de44b264f81ae8855586c0c9323a8e1
Daniel Söderström <daniel.soderstrom@cybercom.com>
2016-12-16 15:07:06 +0100

    placeholder text

--- searchform.php
+++ searchform.php
@@ -8,11 +8,7 @@ $parent_id = advanced_template_top_ancestor();
 	<input type="hidden" value="<?php echo $parent_id; ?>" name="parent" />
 	<?php endif; ?>
 	<div class="input-group">
-		<?php
-		$site_name = get_bloginfo( 'name', 'display' );
-		$search_placeholder = "Sök på $site_name";
-		?>
-		<input type="text" value="<?php echo get_search_query(); ?>" class="form-control" placeholder="<?php echo $search_placeholder ?>" name="s" id="s" />
+		<input type="text" value="<?php echo get_search_query(); ?>" class="form-control" placeholder="<?php _e( 'Vad söker du efter?', 'sk_tivoli' ); ?>" name="s" id="s" />
 		<label class="sr-only" for="s"><?php _e( 'Sök', 'sk_tivoli' ); ?></label>
 		<span class="input-group-btn">
 			<button class="btn btn-secondary" type="submit" id="searchsubmit">
