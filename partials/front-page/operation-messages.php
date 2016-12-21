b8cb32e65fd01864a7e1cf1797744ac2f7eceb8a
Daniel Söderström <daniel.soderstrom@cybercom.com>
2016-12-21 10:39:25 +0100

    added area text to operation message on front.

--- partials/front-page/operation-messages.php
+++ partials/front-page/operation-messages.php
@@ -10,20 +10,26 @@
 						<?php if(!empty($messages)) : ?>
 						<?php foreach ($messages as $message ) : ?>
 							<li class="active">
+								<a href="<?php echo get_permalink( $message->ID ); ?>" title="<?php echo !empty( $message->meta['om_event'][0] ) ? $message->meta['om_event'][0]. ', '  : NULL; ?><?php echo !empty( $message->meta['om_area_street'][0] ) ? $message->meta['om_area_street'][0] . ':' : NULL; ?>">
+								<?php //util::Debug( $message ); ?>
 								<?php material_icon( 'error', array( 'size' => '2em' ) ); ?>
 								<span class="operation-messages-list__om-event">
-									<?php echo !empty( $message->meta['om_event'][0] ) ? $message->meta['om_event'][0] . ':' : NULL; ?>
+									<?php echo !empty( $message->meta['om_event'][0] ) ? $message->meta['om_event'][0]. ', '  : NULL; ?>
+								</span>
+								<span class="operation-messages-list__om-area-street">
+									<?php echo !empty( $message->meta['om_area_street'][0] ) ? $message->meta['om_area_street'][0] . ':' : NULL; ?>
 								</span>
 								<span class="operation-messages-list__om-info">
 									<?php echo !empty( $message->meta['om_information_part_1'][0] ) ? $message->meta['om_information_part_1'][0] : NULL; ?>
 								</span>
+								</a>
 							</li>
 
 						<?php endforeach; ?>
 						<?php else : ?>
 
-						<li class="done"><?php material_icon( 'error', array( 'size' => '2em' ) ); ?> Vi har för
-							närvarande inga driftstörningar
+						<li class="done"><?php material_icon( 'error', array( 'size' => '2em' ) ); ?> <?php _e('Vi har för
+							närvarande inga driftstörningar.', 'msva' );?>
 						</li>
 
 						<?php endif; ?>
@@ -32,7 +38,7 @@
 			</div>
 			<div class="col-md-3">
 				<div class="operation-messages__button">
-					<a class="btn btn-secondary btn-rounded btn-rounded" href="<?php echo get_bloginfo('url') . '/driftstorningar/';?>"><?php _e( 'Gå till alla driftmeddelanden', 'sk_tivoli' );?></a>
+					<a class="btn btn-secondary btn-rounded btn-rounded" href="<?php echo get_bloginfo('url') . '/driftstorningar/';?>" title="<?php _e( 'Arkiv driftstörningar', 'sk_tivoli' );?>"><?php _e( 'Arkiv driftstörningar', 'sk_tivoli' );?></a>
 				</div>
 			</div>
 		</div><!-- .row -->
