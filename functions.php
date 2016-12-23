109bbecf643520320d888e8ee25fa1b360f994ad
Daniel Söderström <daniel.soderstrom@cybercom.com>
2016-12-23 08:17:56 +0100

    refactored code and nonce to ajax object.

--- functions.php
+++ functions.php
@@ -1,16 +1,16 @@
 <?php
 
 function msva_options_page() {
-	if( function_exists('acf_add_options_page') ) {
-		acf_add_options_sub_page(array(
+	if ( function_exists( 'acf_add_options_page' ) ) {
+		acf_add_options_sub_page( array(
 			'page_title'  => 'Inställningar för MSVA',
 			'menu_title'  => 'MSVA',
 			'parent_slug' => 'general-settings'
-		));
+		) );
 	}
 }
-add_filter('init', 'msva_options_page');
 
+add_filter( 'init', 'msva_options_page' );
 
 
 /**
@@ -18,7 +18,7 @@ add_filter('init', 'msva_options_page');
  *
  * @author Daniel Pihlström
  *
- * @since 1.0.0
+ * @since  1.0.0
  *
  */
 function sk_childtheme_enqueue_styles() {
@@ -26,39 +26,54 @@ function sk_childtheme_enqueue_styles() {
 	wp_enqueue_style( 'child-style',
 		get_stylesheet_directory_uri() . '/assets/css/style.css',
 		array( 'main' ),
-		wp_get_theme()->get('Version')
+		wp_get_theme()->get( 'Version' )
 	);
 
-	wp_enqueue_script( 'main-child', get_stylesheet_directory_uri() . '/assets/js/app.js', ['jquery', 'handlebars', 'typeahead'] );
-	wp_localize_script( 'main-child', 'ajax_object', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+	wp_enqueue_script( 'main-child', get_stylesheet_directory_uri() . '/assets/js/app.js', [
+		'jquery',
+		'test',
+		'handlebars',
+		'typeahead'
+	] );
+	wp_enqueue_script( 'test', get_stylesheet_directory_uri() . '/assets/js/source/jquery.easy-autocomplete.js', [
+		'jquery',
+		'handlebars',
+		'typeahead'
+	] );
+
+	wp_localize_script( 'main-child', 'ajax_object', array(
+			'ajaxurl'    => admin_url( 'admin-ajax.php' ),
+			'ajax_nonce' => wp_create_nonce( 'ajax_nonce' )
+		)
+	);
 }
+
 add_action( 'wp_enqueue_scripts', 'sk_childtheme_enqueue_styles' );
 
 /**
  * Get ACF-JSON from parent theme.
  *
  */
-add_filter('acf/settings/save_json', function() {
+add_filter( 'acf/settings/save_json', function () {
 	return get_stylesheet_directory() . '/acf-json';
-});
+} );
 
 
 /**
  * Get ACF json fields from parent theme, child theme and plugins having acf fields
  *
  */
-add_filter('acf/settings/load_json', function( $paths ) {
+add_filter( 'acf/settings/load_json', function ( $paths ) {
 
 	$parent_theme_paths = array( get_template_directory() . '/acf-json' );
-	$paths = array_merge( $paths, $parent_theme_paths );
+	$paths              = array_merge( $paths, $parent_theme_paths );
 
-	if(is_child_theme())
-	{
+	if ( is_child_theme() ) {
 		$paths[] = get_stylesheet_directory() . '/acf-json';
 	}
-	return $paths;
-});
 
+	return $paths;
+} );
 
 
 /**
@@ -85,6 +100,7 @@ $sk_connection_fee = new SK_Connection_Fee();
 /* SK Garbage Scheme */
 require_once locate_template( 'lib/sk-garbage-scheme/class-sk-garbage-scheme.php' );
 $sk_garbage_scheme = new SK_Garbage_Scheme();
+
 
 /* SK Ongoing projects */
 require_once locate_template( 'lib/sk-ongoing-projects/class-sk-ongoing-projects.php' );
