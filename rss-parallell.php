062023231ac54086a93f36e42309b66efd14dad5
Daniel Pihlström <daniel.pihlstrom@cybercom.com>
2018-06-25 10:41:16 +0200

    added custom action to rss feed.

--- rss-parallell.php
+++ rss-parallell.php
@@ -39,7 +39,13 @@ if ( isset( $_GET['parallel_type'] ) && $_GET['parallel_type'] === 'operation-me
 					<dc:creator><?php the_author(); ?></dc:creator>
 					<guid isPermaLink="false"><?php the_guid(); ?></guid>
 					<event>
-						<action><?php echo !empty( $post->meta['om_event'][0] ) ? $post->meta['om_event'][0] : null; ?></action>
+						<?php
+						$type_of_action = $post->meta['om_event'][0];
+						if( $post->meta['om_event'][0] === '1' ){
+							$type_of_action = $post->meta['om_custom_event'][0];
+						}
+						?>
+						<action><?php echo !empty( $type_of_action ) ? $type_of_action : null; ?></action>
 						<municipality><?php echo !empty( $post->meta['om_municipality'][0] ) ? $post->meta['om_municipality'][0] : null; ?></municipality>
 						<title><?php echo !empty( $post->meta['om_title'][0] ) ? $post->meta['om_title'][0] : null; ?></title>
 						<address><?php echo !empty( $post->meta['om_area_street'][0] ) ? $post->meta['om_area_street'][0] : null; ?></address>
