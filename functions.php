83b860d5225742fed6964d11f09e57afc301571e
Daniel Pihlström <daniel.pihlstrom@cybercom.com>
2018-10-05 10:44:49 +0200

    added button to insert shortcode for button link.

--- functions.php
+++ functions.php
@@ -1,4 +1,62 @@
 <?php
+/**
+ * Override parent tiny mce settings.
+ * 
+ * @author Daniel Pihlström <daniel.pihlstrom@cybercom.com>
+ * 
+ * @param integer $toolbar
+ * @return void
+ */
+function get_tinymce_toolbar_items($toolbar = 1) {
+	if ( 1 === intval($toolbar) ) return 'formatselect, bold, link, unlink, blockquote, bullist, numlist, table, spellchecker, eservice_button, youtube_button, sk_collapse, sk_buttonlink, rml_folder';
+	if ( 2 === intval($toolbar) ) return 'pastetext, removeformat, charmap, undo, redo';
+	return false;
+}
+add_filter('tiny_mce_before_init', 'tiny_mce_settings', 99);
+
+function tiny_mce_settings($settings) {
+
+	/**
+	 * Select what to be shown in tinymce toolbars.
+	 *
+	 * Original settings:
+	 *
+	 * toolbar1 = 'bold,italic,strikethrough,bullist,numlist,blockquote,hr,alignleft,aligncenter,alignright,link,unlink,wp_more,spellchecker,fullscreen,wp_adv,eservice_button'
+	 * toolbar2 = 'formatselect,underline,alignjustify,forecolor,pastetext,removeformat,charmap,outdent,indent,undo,redo,wp_help'
+	 */
+	$settings['toolbar1'] = get_tinymce_toolbar_items(1);
+	$settings['toolbar2'] = get_tinymce_toolbar_items(2);
+
+	/**
+	 * Always show toolbar 2
+	 */
+	$settings['wordpress_adv_hidden'] = false;
+
+	/**
+	 * Block formats to show in editor dropdown. We remove h1 as we set page
+	 * title to h1 in the theme.
+	 */
+	$settings['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;';
+
+	return $settings;
+
+}
+
+/**
+ * Adding template path to script.
+ * 
+ * @author Daniel Pihlström <daniel.pihlstrom@cybercom.com>
+ *
+ * @return void
+ */
+function msva_template_dir_js_var() {
+	?>
+	<script>
+		var msvaTemplateDir = "<?php echo get_stylesheet_directory_uri(); ?>";
+	</script>
+	<?php
+}
+add_action('admin_head', 'msva_template_dir_js_var');
 
 
 /**
--- lib/sk-buttonlink/class-sk-buttonlink.php
+++ lib/sk-buttonlink/class-sk-buttonlink.php
@@ -20,8 +20,51 @@ class SK_Buttonlink {
 	 */
 	public function init() {
 		add_shortcode( 'knapplank', array( $this, 'output' ) );
+		add_action('init', array( $this, 'tinymce_buttonlink_button_init'));
 	}
 
+
+	/**
+	 * Add custom button to TinyMCE.
+	 *
+	 * @author Daniel Pihlström <daniel.pihlstrom@cybercom.com>
+	 * 
+	 */
+	public function tinymce_buttonlink_button_init() {
+
+		if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') && get_user_option('rich_editing') == 'true') {
+			return;
+		}
+
+		add_filter('mce_buttons', array($this, 'register_tinymce_buttonlink_button'));
+		add_filter('mce_external_plugins', array($this, 'add_tinymce_buttonlink_button_plugin'));
+	}
+
+	/**
+	 * Register the cutom button.
+	 *
+	 * @author Daniel Pihlström <daniel.pihlstrom@cybercom.com>
+	 * 
+	 * @param [type] $buttons
+	 * @return void
+	 */
+	public function register_tinymce_buttonlink_button($buttons) {
+		$buttons[] = "sk_buttonlink";
+		return $buttons;
+	}
+
+	/**
+	 * Inject the script for the button.
+	 * 
+	 * @author Daniel Pihlström <daniel.pihlstrom@cybercom.com>
+	 *
+	 * @param [type] $plugin_array
+	 * @return void
+	 */
+	public function add_tinymce_buttonlink_button_plugin( $plugin_array ) {
+		$plugin_array['sk_buttonlink'] = get_stylesheet_directory_uri().'/lib/sk-buttonlink/sk-buttonlink.js';
+		return $plugin_array;
+	}
 
 	/**
 	 * HTML output for shortcode
@@ -39,9 +82,9 @@ class SK_Buttonlink {
 
 		$a = shortcode_atts( array(
 			'link' => false,
-			'text'  => false
+			'title'  => false
 		), $atts );
-		printf( '<div class="sk-buttonlink"><h2><a href="%s">%s%s</a></h2></div>', $a['link'], $a['text'], get_material_icon( 'chevron_right', array( 'size' => '200%' ) ) ); 
+		printf( '<div class="sk-buttonlink"><h2><a href="%s">%s%s</a></h2></div>', $a['link'], $a['title'], get_material_icon( 'chevron_right', array( 'size' => '200%' ) ) ); 
 
 		return ob_get_clean();
 
